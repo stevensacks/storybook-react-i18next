7ff5e5984048a98e5c9113dda05aea4ee2516e78
Steven Sacks <stevensacks@gmail.com>
2021-07-12 18:05:49 +0900

    add storybook-i18n

--- preset.js
+++ preset.js
@@ -1,5 +1,9 @@
 function config(entry = []) {
-  return [...entry, require.resolve("./dist/esm/preset/preview")];
+  return [
+    ...entry,
+    require.resolve('storybook-i18n/preview'),
+    require.resolve('./dist/esm/preset/preview'),
+  ];
 }
 
 module.exports = {
