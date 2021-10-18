3fadd98d3a3adf66dbc27b60a970ace24dcd65e8
Steven Sacks <stevensacks@gmail.com>
2021-10-18 13:52:10 +0900

    fix race condition with <Trans> elements with short delay

--- src/preset/preview.tsx
+++ src/preset/preview.tsx
@@ -1,23 +1,31 @@
-import React, { useEffect, Suspense, Fragment } from "react";
+import React, { useEffect, useRef, useState, Suspense, Fragment } from "react";
 import {StoryContext, StoryGetter} from '@storybook/addons/dist/ts3.9/types';
 import {useGlobals} from '@storybook/client-api';
 import {addDecorator} from '@storybook/react';
 import {I18nextProvider} from 'react-i18next';
 
 const withI18Next = (story: StoryGetter, context: StoryContext) => {
-  const [{locale}] = useGlobals();
   const {
     parameters: {i18n},
   } = context;
 
+  const [{locale}] = useGlobals();
+  const [show, setShow] = useState(true);
+  const timeoutRef = useRef(null);
+
   useEffect(() => {
     if (locale) {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      setShow(false);
       i18n?.changeLanguage(locale);
+      timeoutRef.current = setTimeout(() => setShow(true), 100);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [locale]);
 
-  if (i18n) {
+  if (i18n && show) {
     return (
       <Suspense fallback="Loading...">
         <Fragment key={locale}>
