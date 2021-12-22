754b5b6e5d4715f61b756c2f7a12f4fb05f757c8
Steven Sacks <stevensacks@gmail.com>
2021-12-22 16:17:10 +0900

    chore(deps): update to storybook 6.4

---
+++ src/preset/preview.ts
@@ -0,0 +1,3 @@
+import {withI18Next} from '../withI18Next';
+
+export const decorators = [withI18Next];
---
+++ src/withI18Next.tsx
@@ -0,0 +1,46 @@
+import React, {useEffect, useRef, useState, Suspense, Fragment} from 'react';
+import {
+    AnyFramework,
+    PartialStoryFn as StoryFunction,
+    StoryContext,
+} from '@storybook/csf';
+import {useGlobals} from '@storybook/client-api';
+import {I18nextProvider} from 'react-i18next';
+
+export const withI18Next = (
+    story: StoryFunction<AnyFramework>,
+    context: StoryContext
+) => {
+    const {
+        parameters: {i18n},
+    } = context;
+
+    const [{locale}] = useGlobals();
+    const [show, setShow] = useState(true);
+    const timeoutRef = useRef(null);
+
+    useEffect(() => {
+        if (locale) {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+            setShow(false);
+            i18n?.changeLanguage(locale);
+            timeoutRef.current = setTimeout(() => setShow(true), 100);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [locale]);
+
+    if (i18n && show) {
+        return (
+            <Suspense fallback="Loading...">
+                <Fragment key={locale}>
+                    <I18nextProvider i18n={i18n}>
+                        {story(context)}
+                    </I18nextProvider>
+                </Fragment>
+            </Suspense>
+        );
+    }
+    return story(context);
+};
